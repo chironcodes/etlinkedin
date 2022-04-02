989863abbb3e149877a34811ed6fbbe51a549c26
diego <diego@pop-os.localdomain>
2022-04-02 19:06:53 -0300

    Apply PEP8

--- modules/connector.py
+++ modules/connector.py
@@ -1,9 +1,39 @@
+"""
+    Class to connect to SQLite database
+    Through this class :class:`InterfaceDB`, we can make the connection
+    with the database SQLite, query data and insert data.
+    To use the :class:`InterfaceDB`, the implementation occurs as follows:
+    .. autoattribute:: builder
+    :annotation:
+    Example of the constructor method
+    ----------------------------
+    >>> object = InterfaceDB(host, database)
+    Method to connect to the database
+    ----------------------------
+    >>> object.get_conn(self)
+    Method to close the connection in the database
+    ----------------------------
+    >>> object.close_conn(self, conn, curso)
+    Method to create a table
+    ----------------------------
+    >>> object.create_table(self, create_table_sql)
+    Method to insert record in table
+    ----------------------------
+    >>> object.insert_into(self, table: str, **kwargs)
+    xxxxx
+    ----------------------------
+    >>> object.get_id_by_any_from_any(self, table: str, **kwargs)
+    xxxxx
+    ----------------------------
+    >>> object.get_id_by_any_from_any(self, table: str, **kwargs)
+    """
+
 import sqlite3
 from sqlite3 import Error
 
 
-class interface_db:
-    
+class InterfaceDB():
+
     def __init__(self, database):
         try:
             self.database = database
@@ -18,8 +48,7 @@ class interface_db:
             return conn, cursor
         except Exception as e:
             print(str(e))
-            
-            
+
     def close_conn(self, conn, cursor):
         try:
             cursor.close()
@@ -27,38 +56,24 @@ class interface_db:
             conn.close()
         except Exception as e:
             print(str(e))
-            
+
     def create_table(self, create_table_sql):
-        """ create a table from the create_table_sql statement
-        :param conn: Connection object
-        :param create_table_sql: a CREATE TABLE statement
-        :return:
-        """
         try:
             conn, cursor = self.get_conn()
             cursor.execute(create_table_sql)
             self.close_conn(conn, cursor)
         except Error as e:
             print(e)
-            
-            
-            
-            
-            
-            
-    #░▄▀▀░░▒█▀▄░░░█▒█░░░█▀▄░░▒█▀░█▒█░█▄░█░▄▀▀░▀█▀░█░▄▀▄░█▄░█░▄▀▀
-    #░▀▄▄░▄░█▀▄░▄░▀▄█░▄▒█▄▀▒░░█▀░▀▄█░█▒▀█░▀▄▄░▒█▒░█░▀▄▀░█▒▀█▒▄██
 
-    
-    def insertInto(self, table:str, **kwargs):
+    def insert_into(self, table: str, **kwargs):
         try:
             conn, cursor = self.get_conn()
-            columns=""
-            values=""
-            count =1
+            columns = ""
+            values = ""
+            count = 1
             for kwkey, kwvalue in kwargs.items():
-                count+=1
-                columns+="{0}, ".format(kwkey)
+                count += 1
+                columns += "{0}, ".format(kwkey)
                 if isinstance(kwvalue, int):
                     values += "{0}, ".format(kwvalue)
                 else:
@@ -67,28 +82,24 @@ class interface_db:
             columns = columns[:-2]
             values = values[:-2]
 
-            query = "INSERT INTO {} ({}) VALUES ({})".format(table,columns,values)
+            query = f"INSERT INTO {table} ({columns}) VALUES ({values})"
             print(query)
             cursor.execute(query)
-            self.close_conn(conn,cursor)
+            self.close_conn(conn, cursor)
         except Exception as e:
             print(str(e))
-    
-    def get_id_by_any_from_any(self, table:str, **kwargs):
-        # usage sample
-        # get_id_by_any_from_any("actor", by="actor_id", condition="actor_url", value="http://www.profile/url")
+
+    def get_id_by_any_from_any(self, table: str, **kwargs):
         try:
             conn, cursor = self.get_conn()
-            if isinstance(kwargs.get("value"), int):    
+            if isinstance(kwargs.get("value"), int):
                 query = "SELECT {} FROM {} WHERE {}={}" \
-                .format(kwargs.get("by"), table,kwargs.get("condition"),kwargs.get("value"))
+                    .format(kwargs.get("by"), table, kwargs.get("condition"), kwargs.get("value"))
             else:
                 query = "SELECT {} FROM {} WHERE {}='{}'" \
-                .format(kwargs.get("by"), table,kwargs.get("condition"),kwargs.get("value"))
+                    .format(kwargs.get("by"), table, kwargs.get("condition"), kwargs.get("value"))
             print(query)
-            self.close_conn(conn,cursor)
+            self.close_conn(conn, cursor)
             return cursor.fetchall()
         except Exception as e:
             print(str(e))
-        
-        
