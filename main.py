d2212193fef491ec50ec223970c8e3b3b95bf25f
chironcodes <adiltoncss@gmail.com>
2022-04-02 20:24:51 -0300

    the final MVP

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from bs4 import BeautifulSoup as bs
 
 from beau_selekdin.modules.connector import interface_db
 
@@ -22,7 +23,6 @@ today = date.today()
 desired = ['experience',
            'education',
            'licenses_and_certifications',
-           'skills',
            'courses']
 
 root_url = "https://www.linkedin.com"
@@ -71,8 +71,8 @@ def treat_date(date_string: str):
         exp_beg = exp_beg.split(" ")[2]+mes[exp_beg.split(" ")[0]]
         exp_end = date_string.split("-")[1][:]
         if exp_end.__contains__("o momento"):
-            current_year = today.year
-            current_month = today.month
+            current_year = today.strftime("%Y")
+            current_month = today.strftime("%m")
             exp_end = current_year + "." + current_month
             # exp_end = "2022.04"
         else:
@@ -83,87 +83,109 @@ def treat_date(date_string: str):
         return " " 
 
 
-def scrap_me(table: str, actor_id: int, section):
-    count = 0
+def scrap_me(table:str, actor_id:int, section):
+    count=0
     cards = section.select("li.artdeco-list__item")
     for card in cards:
-        count = count+1
-        print("$$$$contadorrr --", count)
-        print("####tamanho da lista---", len(card.find_all(attrs={"tabindex": "-1"})))
-        # If is someone with a big story in the company...
-        if (len(card.find_all(attrs={"tabindex": "-1"}))>1): 
-            exp_company = card.find_all(attrs={"aria-hidden": "true"})[0].text
+            count=count+1
+            # If is someone with a big story in the company...
+            if (len(card.find_all(attrs={"tabindex": "-1"}))>1):
+                exp_company = card.find_all(attrs={"aria-hidden": "true"})[0].text
 
-            if table=='experience': 
-                all_exp_name = card.select("div.display-flex>span.mr1, t-bold")
-                exp_company = all_exp_name[0].span.text
-                list_exp_name = [
-                    (i.contents[1].text) for i in all_exp_name[1:]
-                ]
-                all_exp_date = card.select("div>*>span:nth-of-type(1).t-14 ,t-normal, t-black--light")
-                list_exp_date = [
-                    (i.contents[1].text) for i in all_exp_date[1:]
-                ]
-                for exp_name, exp_date in zip(list_exp_name, list_exp_date): 
-                    print("INSERCAO_LISTA", exp_date)
+                if table=='experience':
+                    all_exp_name = card.select("div.display-flex>span.mr1, t-bold")
+                    exp_company = all_exp_name[0].span.text
+                    list_exp_name = [
+                        (i.contents[1].text) for i in all_exp_name[1:]
+                    ]
+                    all_exp_date = card.select("div>*>span:nth-of-type(1).t-14 ,t-normal, t-black--light")
+                    list_exp_date = [
+                        (i.contents[1].text) for i in all_exp_date[1:]
+                    ]
+                    for exp_name, exp_date in zip(list_exp_name,list_exp_date):
+                        exp_beg, exp_end = treat_date(exp_date)
+                        social_data.insertInto(table, actor_id=actor_id, \
+                            exp_name=exp_name, exp_company=exp_company, \
+                            exp_beg=exp_beg, exp_end=exp_end)                                          
+                elif table=='education':
+                    print(" 33rr....you reached a list of ",table," adding content for actor_id ",id," ... you shouldn'be here, tho...")                    
+                elif table=='licenses_and_certifications':
+                    print(" 33rr....you reached a list of ",table," adding content for actor_id ",id," ... you shouldn'be here, tho...")                                       
+                elif table=='courses':
+                    print(" 33rr....you reached a list of ",table," adding content for actor_id ",id," ... you shouldn'be here, tho...")
+                elif table=='skills':
+                    print(" 33rr....you reached a list of ",table," adding content for actor_id ",id," ... you shouldn'be here, tho...")
+
+            else:
+                if table=='experience':
+                    try:
+                        exp_name = card.select("span.mr1, t-bold")[0].span.text
+                    except Exception as e:
+                        exp_name = " "
+                    try:
+                        exp_company = card.select("span.t-14, t-normal")[0].span.text
+                    except Exception as e:
+                        exp_company = " "
+                    try:
+                        exp_date = card.select("span.t-14, t-normal")[1].span.text
+                    except Exception as e:
+                        exp_date = " "
+                        
                     exp_beg, exp_end = treat_date(exp_date)
-                    social_data.insertInto(table, actor_id=actor_id,
-                        exp_name=exp_name, exp_company=exp_company,
-                        exp_beg=exp_beg, exp_end=exp_end)                                
-            elif table=='education': 
-                print(" 33rr....you reached a list of ", table, " adding content for actor_id ", id, " ... you shouldn'be here, tho...") 
-            elif table=='licenses_and_certifications': 
-                print(" 33rr....you reached a list of ", table, " adding content for actor_id ", id, " ... you shouldn'be here, tho...")                                     
-            elif table=='courses': 
-                print(" 33rr....you reached a list of ", table, " adding content for actor_id ", id, " ... you shouldn'be here, tho...")
-            elif table=='skills': 
-                print(" 33rr....you reached a list of ", table, " adding content for actor_id ", id, " ... you shouldn'be here, tho...")
-
-        else:
-            if table=='experience':
-                exp_name = card.select("span.mr1, t-bold")[0].span.text
-                exp_company = card.select("span.t-14, t-normal")[0].span.text
-                exp_date = card.select("span.t-14, t-normal")[1].span.text
-                print("INSERCAO_UNICA", exp_date)
-                exp_beg, exp_end = treat_date(exp_date)
-                social_data.insertInto('experience', actor_id=actor_id,
-                    exp_name=exp_name, exp_company=exp_company,
-                    exp_beg=exp_beg, exp_end=exp_end)  
-                
-            elif table=='education':
-                edu_company = card.select("span.mr1, t-bold")[0].span.text if card.select("span.mr1, t-bold")[0].span.text != "" else edu_company = ""
-                
-                edu_company = card.select("span.mr1, t-bold")[0].span.text
-                
-                edu_title = card.select("span.t-14, t-normal")[0].span.text
-                exp_date = card.select("span.t-14, t-normal")[1].span.text
-                edu_beg, edu_end = treat_date(exp_date)
-                social_data.insertInto('education', actor_id=actor_id, \
-                        edu_company=edu_company, edu_title=edu_title, \
-                        edu_beg=edu_beg, edu_end=edu_end) 
-                
-            elif table=='licenses_and_certifications': 
-                mes ={'jan': '.01', 'fev': '.02', 'mar': '.03', 'abr': '.04', 'mai': '.05', 'jun': '.06', 'jul': '.07', 'ago': '.08', 'set': '.09', 'out': '.10', 'nov': '.11', 'dez': '.12'}
-                lice_name = card.select("span.mr1, t-bold")[0].span.text
-                lice_company = card.select("span.t-14, t-normal")[0].span.text
-                lice_when = card.select("span.t-14, t-normal")[1].span.text
-                if (len(lice_when)>10):
-                    lice_when = lice_when.split("·")[0][11:]
-                    lice_when = lice_when.split(" ")[2]+mes[lice_when.split(" ")[0]]    
-                else:
-                    lice_when = ""
-                social_data.insertInto('li_and_cert', actor_id=actor_id,
-                        lice_name=lice_name, lice_company=lice_company,
-                        lice_when=lice_when)  
-
-            elif table == 'courses': 
-                course_name = card.select("span.mr1, t-bold")[0].span.text
-                social_data.insertInto('course', actor_id=actor_id,
-                        course_name=course_name)  
-
-            elif table=='skills': 
-                print("coming soon...")
-        print("~~~~~\n\n\n")  
+                    social_data.insertInto('experience', actor_id=actor_id, \
+                        exp_name=exp_name, exp_company=exp_company, \
+                        exp_beg=exp_beg, exp_end=exp_end)  
+                    
+                elif table=='education':
+                    try:
+                        edu_company = card.select("span.mr1, t-bold")[0].span.text
+                    except Exception as e:
+                        edu_company = " "
+                    try:
+                        edu_title = card.select("span.t-14, t-normal")[0].span.text
+                    except Exception as e:
+                        edu_title = " "
+                    try:
+                        exp_date = card.select("span.t-14, t-normal")[1].span.text
+                    except Exception as e:
+                        exp_date = " "
+                        
+                    edu_beg, edu_end = treat_date(exp_date)
+                    social_data.insertInto('education', actor_id=actor_id, \
+                            edu_company=edu_company, edu_title=edu_title, \
+                            edu_beg=edu_beg, edu_end=edu_end) 
+                    
+                elif table=='licenses_and_certifications':
+                    mes ={'jan':'.01','fev':'.02','mar':'.03','abr':'.04','mai':'.05','jun':'.06','jul':'.07','ago':'.08','set':'.09','out':'.10','nov':'.11','dez':'.12'}
+                    try:
+                        lice_name = card.select("span.mr1, t-bold")[0].span.text
+                    except Exception as e:
+                        lice_name = " "
+                    try:
+                        lice_company = card.select("span.t-14, t-normal")[0].span.text
+                    except Exception as e:
+                        lice_company = " "
+                    try:
+                        lice_when = card.select("span.t-14, t-normal")[1].span.text
+                    except Exception as e:
+                        lice_when = " "
+                        
+                    if (len(lice_when)>10):
+                        lice_when = lice_when.split("·")[0][11:]
+                        lice_when = lice_when.split(" ")[2]+mes[lice_when.split(" ")[0]]    
+                    else:
+                        lice_when = ""
+                    social_data.insertInto('li_and_cert', actor_id=actor_id, \
+						    lice_name=lice_name, lice_company=lice_company, \
+						    lice_when=lice_when)  
+                    
+                elif table=='courses':
+                    try:
+                        course_name = card.select("span.mr1, t-bold")[0].span.text
+                    except Exception as e:
+                        course_name = " "
+                    social_data.insertInto('course', actor_id=actor_id, \
+                            course_name=course_name) 
 
 
 if __name__ == "__main__":
@@ -191,5 +213,24 @@ if __name__ == "__main__":
     profile_urls = [url.get_attribute('href').split("/in/")[1][:-1] for url in profile_urls if url.get_attribute('href').__contains__("/in/")]
 
     # filters person's accounts only and feed them to the sqlite
+    
     for url in profile_urls:
-        social_data.insertInto("actor", actor_url=url)
+        # builds actor_url
+        actor_url=root_url+"/in/"+url
+        driver.get(actor_url)
+        slow_down()
+        
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        newHeight = driver.execute_script("return document.body.scrollHeight")
+        profile = bs(driver.page_source.encode("utf-8"), "lxml")
+        
+        all_sections = profile.find_all("section", class_="artdeco-card ember-view break-words pb3 mt4")
+        
+        # adds and returns id
+        inserted_id = social_data.insertInto('actor', actor00_url=actor_url)
+        
+        for section in all_sections:
+            section_name = section.div.get("id")
+            if(section_name in desired):
+                print(section_name)
+                scrap_me(section_name, inserted_id, section)
