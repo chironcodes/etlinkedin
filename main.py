6951c36f07d85e7d843955bd5b2f81f7c8a58019
chironcodes <adiltoncss@gmail.com>
2022-04-03 17:34:28 -0300

    implementing WebDriverWait for a more dinamic and perfomatic run

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from random import uniform
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup as bs
@@ -42,10 +44,16 @@ if __name__ == "__main__":
     load_all_actors(driver)
     profile_urls = driver.find_elements(By.CLASS_NAME, "follows-recommendation-card__avatar-link")
     profile_urls = [url.get_attribute('href').split("/in/")[1][:-1] for url in profile_urls if url.get_attribute('href').__contains__("/in/")]
-    for url in profile_urls:
+    for url in profile_urls:       
         actor_url = root_url+"/in/"+url
         driver.get(actor_url)
-        slow_down()
+        try:
+            elem = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.XPATH, "//*[@aria-label='Mais ações']"))
+            )
+        except Exception as e:
+            print(str(e))
+            time.sleep(3)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         newHeight = driver.execute_script("return document.body.scrollHeight")
         profile = bs(driver.page_source.encode("utf-8"), "lxml")
